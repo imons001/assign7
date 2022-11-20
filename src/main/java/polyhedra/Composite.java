3aa77a078b95832c9cffcd5a8d0da23c9a6bd061
imons001 <94793634+imons001@users.noreply.github.com>
2022-11-19 21:25:15 -1000

    Add files via upload

--- src/main/java/polyhedra/Composite.java
+++ src/main/java/polyhedra/Composite.java
@@ -77,10 +77,8 @@ public class Composite extends Polyhedron
     	for (int i = 0; i < numPolyhedra; i++) {
     		Polyhedron newPolyhedron = PolyhedronFactory.createAndRead(scanner);
     		this.allPolyhedra.add(newPolyhedron);
+    		this.boundingBox = newPolyhedron.getBoundingBox();
     	}
-    	
-    	this.boundingBox.merge(newPolyhedron.getBoundingBox());
-    	
     }
 
     /**
